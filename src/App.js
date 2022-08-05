6a0775f517540b4f0efd846485234fecb692d5cd
Laksika <laksika.0175705@gmail.com>
2022-08-05 14:16:56 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+const word = "Hello";
 function App() {
- return (
- <div>
- Hello World
- </div>
- );
+    return (
+        <div>
+           <WordCard value = {word}/>
+
+        </div>
+    );
 }
 export default App;
