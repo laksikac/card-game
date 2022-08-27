2c8ca4ed3784c6dd42e161afcf93dfa3c60d6981
can <laksika.0175705@gmail.com>
2022-08-27 16:27:16 +0700

    add popup correct word

--- src/Score.js
+++ src/Score.js
@@ -1,17 +1,37 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
+
 
 
 export default function Score(props){
-    //let message = '';
+ 
     const className = `cardScore `
+    if(props.attempt == 0){
+       window.location.reload();
+    }
    
     
     return (
-       
+       <div>
+   
         <div className={className}>
-            <span>Score: {props.score}</span>
+            <span>word: {props.guess}</span>
+        </div >
+        {
+            props.completed? 
+            <div className="popUp">
+                <span>
+                Congratulations!, this word is {props.word}
+            </span>
+            </div>
+            :null
+        }
+        <div className={className}>
+            <span>attempt: {props.attempt}</span>
         </div>
-        
+         <div className={className}>
+         <span>Score: {props.score}</span>
+      </div>
+      </div>
         
         
       
